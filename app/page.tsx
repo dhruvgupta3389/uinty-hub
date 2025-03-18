f2fd015046e249f51b552ed9ee776e5fafc38f01
Dhruv Gupta <146109043+dhruvgupta3389@users.noreply.github.com>
2025-03-18 18:24:29 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { CTASection } from '@/components/sections/cta';
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <HeroSection />
+{/*       <HeroSection /> */}
       <FeaturesSection />
       <DemoSection />
       <HowItWorksSection />
